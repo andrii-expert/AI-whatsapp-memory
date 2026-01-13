1d50fc59bac55ea8dd317ae6a4f28d85823eb5e2
andrii-expert <andrciganchuk@gmail.com>
2026-01-13 09:37:43 +0200

    remove x button

--- apps/user-portal/src/components/share-details-modal.tsx
+++ apps/user-portal/src/components/share-details-modal.tsx
@@ -472,12 +472,6 @@ export function ShareDetailsModal({
                   People who have access to "{resourceName}"
                 </AlertDialogDescription>
               </div>
-              <button
-                onClick={onClose}
-                className="text-gray-500 hover:text-gray-700 transition-colors ml-4"
-              >
-                <X className="h-5 w-5" />
-              </button>
             </div>
           </AlertDialogHeader>
 
